89d09c3c78b9126d58151574e0ae7eb3521074de
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-10-17 11:03:28 +0530

    Added support for POST method

--- src/app/todo-app.js
+++ src/app/todo-app.js
@@ -1,11 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 const rocket = require('../lib/rocket');
 const app = rocket();
 const port = 3000;
@@ -17,17 +9,24 @@ let data = [
 ];
 
 
+// Get all todos
 app.get('/todos', (req, res) => {
-    res.send(`Getting todos: ${JSON.stringify(data)}`);
+    res.send(JSON.stringify(data));
 });
 
+// Get with param
 app.get('/todos/:id', function (req, res) {
     objIndex = data.findIndex((obj => obj.id == req.params.id));
-
     let todo = data[objIndex].item;
-
-    res.send(`${JSON.stringify(todo)}`);
+    res.send(JSON.stringify(todo));
 });
+
+// New todo 
+app.post('/todo', function (req, res) {
+    data.push(req.body);
+    res.send(JSON.stringify(data));
+});
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
