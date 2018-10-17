832f3a4042346a2d597d3238f16a1f9879cd0408
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-10-17 09:33:06 +0530

    Creating the basic app.js file

--- src/lib/app.js
+++ src/lib/app.js
@@ -2,3 +2,38 @@ let Router = require('./router'),
     http = require('http'),
     Response = require('./response');
 
+
+class App {
+    constructor() {
+        this.router = new Router();
+        this.methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+        this.init();
+    }
+
+    init() {
+        // Create method on the instance of app
+        this.methods.forEach((methods) => {
+            this[method.toLowerCase()] = (path, callback) => {
+                this.router.route(method, path, callback);
+            }
+        });
+    }
+
+    handle(req, res) {
+        res.__proto__ = Response.prototype;
+        res.app = this;   // used in res.render
+        this.router.handle(req, res);
+    }
+
+    listen() {
+        let server = http.createServer((req, res) => {
+            this.handle(req, res);
+        });
+
+        server.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    }
+}
+
+module.exports = App;
