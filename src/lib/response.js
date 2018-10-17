8b477b75e6fab2407cf70243c5be2c73d7a02b96
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-10-17 13:13:10 +0530

    Refactored response object to es6 class

--- src/lib/response.js
+++ src/lib/response.js
@@ -2,64 +2,60 @@ var http = require('http')
 
 // Our own Response class.
 // We make `res` inherit from this to add our own helper methods.
-function Response() { }
-
-Response.prototype = Object.create(http.ServerResponse.prototype)
-Response.prototype.constructor = Response
-
-
-// Default Content-Type to HTML.
-Response.prototype.contentType = 'text/html'
-
-// Helper to send a response.
-// Usage:
-//   res.send('body')
-//   res.send(404, 'Not found')
-Response.prototype.send = function (status, body) {
-    if (body == null) {
-        body = status;   // If only one parameter
-        status = this.statusCode || 200;
+class Response extends http.ServerResponse {
+    constructor() {
+        super();
     }
 
-    let contentLength = body ? body.length : 0;
-    this.writeHead(status, {
-        'Content-Length': contentLength,
-        'Content-Type': this.contentType
-    })
-    this.end(body)
+    send(status, body) {
+        if (body == null) {
+            body = status;   // If only one parameter
+            status = this.statusCode || 200;
+        }
+
+        let contentLength = body ? body.length : 0;
+        console.log("this: ", this.contentType);
+        this.writeHead(status, {
+            'Content-Length': contentLength,
+            'Content-Type': this.contentType
+        })
+        this.end(body)
+    }
+
+    json(body) {
+        this.contentType = "application/json";
+        body = JSON.stringify(body);
+        this.send(body);
+    }
+
+    status(code) {
+        this.statusCode = code;
+        return this;
+    }
+
+    notFound(code) {
+        this.statusCode = 404;
+        return this;
+    }
+
+    redirect(url) {
+        // writeHead comes from http (response)
+        this.writeHead(302, {
+            'Location': url
+            //add other headers here...
+        });
+        this.end();
+    }
+
+    render(file, locals = {}) {
+        var self = this
+        // Invoke render of the app
+        this.app.render(file, locals, function (html) {
+            self.send(html)
+        })
+    }
 }
+Response.prototype.contentType = 'text/html'
 
-Response.prototype.status = function status(code) {
-    this.statusCode = code;
-    return this;
-};
-
-Response.prototype.notFound = function notFound(code) {
-    this.statusCode = 404;
-    return this;
-};
-
-Response.prototype.json = function (body) {
-    this.contentType = "application/json";
-    body = JSON.stringify(body);
-    this.send(body);
-}
-
-Response.prototype.redirect = function (url) {
-    // writeHead comes from http (response)
-    this.writeHead(302, {
-        'Location': url
-        //add other headers here...
-    });
-    this.end();
-}
-
-Response.prototype.render = function (file, locals = {}) {
-    var self = this
-    // Invoke render of the app
-    this.app.render(file, locals, function (html) {
-        self.send(html)
-    })
-}
 
 module.exports = Response;
