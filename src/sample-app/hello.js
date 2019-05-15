f81cccc170983c5ad198041a8c4ca139889dd503
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-05-15 16:29:45 +0530

    Refactored example.  Patch method not working.  TODO: FIX

--- src/sample-app/hello.js
+++ src/sample-app/hello.js
@@ -10,13 +10,38 @@ const rocket = require('../lib/rocket');
 const app = rocket();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/welcome', (req, res) => res.send('Hello World -> welcome!'));
-app.get('/:name', (req, res) => res.send(`Hello World -> ${req.params.name}`));
-app.get('/user/:name/edit', (req, res) => res.send(`Hello user edit -> ${req.params.name}`));
+app.get('/', (req, res) => res.send('Our own tiny node server (rocket)'));
+
+app.get('/welcome', (req, res) => res.send('Welcome !'));
+
+app.get('/:name', (req, res) => {
+    res.send(`Hello -> ${req.params.name}`);
+});
+
+app.get('/user/:name/edit', (req, res) => {
+    res.send(`Editing user ${req.params.name}`);
+});
+
+// Create new resource
+app.post('/user', (req, res) => {
+    console.log("POST:/user/new", req.body);
+    res.json(req.body);
+});
+
+// Create or modify existing resource
+app.put('/user', (req, res) => {
+    console.log("PUT:/user/new", req.body);
+    res.json(req.body);
+});
+
+// Minor updates to existing resource
+app.patch('/user/:username', (req, res) => {
+    console.log("PATCH:/user", req.body.username);
+    res.json(req.body);
+});
 
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`)
+);
 
